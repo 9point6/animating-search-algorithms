bcabdede8daf5988ce7d29ee3f641fa0388418dc
John Sanderson <clickheretoemailjohn@gmail.com>
2011-11-02 12:54:52 +0000

    updated pre-comiple JS

--- script/algorithm.js
+++ script/algorithm.js
@@ -1,22 +1,20 @@
-(function() {
-  var algorithm;
-  algorithm = (function() {
-    algorithm.prototype.root_node = null;
-    algorithm.prototype.goal_node = null;
-    algorithm.prototype.explored_nodes = [];
-    algorithm.prototype.traverse_info = [];
-    function algorithm() {
-      false;
-    }
-    algorithm.prototype.search = function() {
-      throw "Search not implemented";
-    };
-    algorithm.prototype.gen_info = function() {
-      throw "General info not implemented";
-    };
-    algorithm.prototype.run_info = function() {
-      throw "Run info not implemented";
-    };
-    return algorithm;
-  })();
-}).call(this);
+var algorithm;
+algorithm = (function() {
+  algorithm.prototype.root_node = null;
+  algorithm.prototype.goal_node = null;
+  algorithm.prototype.explored_nodes = [];
+  algorithm.prototype.traverse_info = [];
+  function algorithm() {
+    false;
+  }
+  algorithm.prototype.search = function() {
+    throw "Search not implemented";
+  };
+  algorithm.prototype.gen_info = function() {
+    throw "General info not implemented";
+  };
+  algorithm.prototype.run_info = function() {
+    throw "Run info not implemented";
+  };
+  return algorithm;
+})();
--- script/animate.js
+++ script/animate.js
@@ -1,26 +1,24 @@
-(function() {
-  var animate;
-  animate = (function() {
-    function animate(raphael) {
-      this.raphael = raphael;
-      this.r = this.raphael;
+var animate;
+animate = (function() {
+  function animate(raphael) {
+    this.raphael = raphael;
+    this.r = this.raphael;
+  }
+  animate.prototype.traverse = function(algorithm) {
+    var connection, _i, _len, _ref, _results;
+    _ref = algorithm.explored_connections;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      connection = _ref[_i];
+      _results.push(connection.p);
     }
-    animate.prototype.traverse = function(algorithm) {
-      var connection, _i, _len, _ref, _results;
-      _ref = algorithm.explored_connections;
-      _results = [];
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        connection = _ref[_i];
-        _results.push(connection.p);
-      }
-      return _results;
-    };
-    animate.prototype.node = function(node, colour, size) {
-      return false;
-    };
-    animate.prototype.connection = function(connection, colour) {
-      return false;
-    };
-    return animate;
-  })();
-}).call(this);
+    return _results;
+  };
+  animate.prototype.node = function(node, colour, size) {
+    return false;
+  };
+  animate.prototype.connection = function(connection, colour) {
+    return false;
+  };
+  return animate;
+})();
