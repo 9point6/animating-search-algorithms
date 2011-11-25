d968499359cc3ac4460c28588f24b4a14b4dcdad
Jack Histon <histon666@gmail.com>
2011-11-25 21:25:48 +0000

    Made BFS work :)

--- script/BFS.coffee
+++ script/BFS.coffee
@@ -1,38 +1,59 @@
 class BFS extends Algorithm
     name: "Beadth-First Search"
 
+    destroy: ->
+        for node in @explored_nodes
+            delete node.explored
+        super
+
     search: ->
+        for node in @explored_nodes
+            node.explored = false
+
+        @explored_nodes = []
+        @traverse_info = []
+
         #queue of nodes to be searched
         queue = []
 
         #add the root node to the front of the queue
-        queue.push root_node
+        queue.push @root_node
 
         #add the root node to the set of explored nodes
-        explored_nodes.push root_node
+        @root_node.explored = true
 
         #if the root node is the goal then end search
-        if root_node is goal_node
+        if @root_node is @goal_node
             return
 
         #while there are still nodes in the queue
-        while queue.length is not 0
+        while queue.length isnt 0
 
             #the new root node is the first node in the queue
-            root_node = queue.shift()
+            current_node = queue.shift()
+
+            if current_node is @goal_node
+                @explored_nodes.push current_node
+                @traverse_info.push current_node
+                break
 
             #get the connections of the node
-            neighbours = root_node.connections
+            neighbours = current_node.connections
+
+            @traverse_info.push current_node
 
             #for all the neighbours of the node
             for neighbour in neighbours
                 #add the neighbour to the set of explored nodes
-                explored_nodes.push neighbour.p
-                queue.push neighbour.p
+                if not neighbour.p.explored
+                    neighbour.p.explored = true
+                    queue.push neighbour.p
+                    @traverse_info.push neighbour.c
 
-                #if the nieghbour is the goal node end search
-                if neighbour.p is goal_node
-                    break
+            @explored_nodes.push current_node
+
+    create_traverse_info: ->
+        false
 
     gen_info: ->
         [
--- script/BFS.js
+++ script/BFS.js
@@ -14,34 +14,53 @@
       BFS.__super__.constructor.apply(this, arguments);
     }
     BFS.prototype.name = "Beadth-First Search";
+    BFS.prototype.destroy = function() {
+      var node, _i, _len, _ref;
+      _ref = this.explored_nodes;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        node = _ref[_i];
+        delete node.explored;
+      }
+      return BFS.__super__.destroy.apply(this, arguments);
+    };
     BFS.prototype.search = function() {
-      var neighbour, neighbours, queue, root_node, _results;
+      var current_node, neighbour, neighbours, node, queue, _i, _j, _len, _len2, _ref, _results;
+      _ref = this.explored_nodes;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        node = _ref[_i];
+        node.explored = false;
+      }
+      this.explored_nodes = [];
+      this.traverse_info = [];
       queue = [];
-      queue.push(root_node);
-      explored_nodes.push(root_node);
-      if (root_node === goal_node) {
+      queue.push(this.root_node);
+      this.root_node.explored = true;
+      if (this.root_node === this.goal_node) {
         return;
       }
       _results = [];
-      while (queue.length === !0) {
-        root_node = queue.shift();
-        neighbours = root_node.connections;
-        _results.push((function() {
-          var _i, _len, _results2;
-          _results2 = [];
-          for (_i = 0, _len = neighbours.length; _i < _len; _i++) {
-            neighbour = neighbours[_i];
-            explored_nodes.push(neighbour.p);
+      while (queue.length !== 0) {
+        current_node = queue.shift();
+        if (current_node === this.goal_node) {
+          this.explored_nodes.push(current_node);
+          this.traverse_info.push(current_node);
+          break;
+        }
+        neighbours = current_node.connections;
+        this.traverse_info.push(current_node);
+        for (_j = 0, _len2 = neighbours.length; _j < _len2; _j++) {
+          neighbour = neighbours[_j];
+          if (!neighbour.p.explored) {
+            neighbour.p.explored = true;
             queue.push(neighbour.p);
-            if (neighbour.p === goal_node) {
-              break;
-            }
+            this.traverse_info.push(neighbour.c);
           }
-          return _results2;
-        })());
+        }
+        _results.push(this.explored_nodes.push(current_node));
       }
       return _results;
     };
+    BFS.prototype.create_traverse_info = function() {};
     BFS.prototype.gen_info = function() {
       return ["Complete", "O(b<sup>d+1</sup>)", "O(b<sup>d</sup>)", "Not Optimal"];
     };
